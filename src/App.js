710f67f6a6850d58862600d781697946da74cb37
simer1991 <satinder0728@gmail.com>
2022-06-09 16:28:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './compoents';
+import {Home , About} from './Pages';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Updated version
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+      </Router>
+    
   );
 }
 
---
+++ src/compoents/header/Header.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './header.css';
+import { Container, Nav, Navbar, NavDropdown, Row } from 'react-bootstrap-v5';
+import {FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn} from 'react-icons/fa';
+import Logo from '../../assets/images/mxr_logo.png';
+import { NavLink } from 'react-router-dom';
+
+
+const Header = () => {
+  return (
+      <>
+      <div id="wrapper" className="page-wrapper">
+        <div class="mxr__header">
+            <div className='mxr__header-top'>
+                <Container>           
+                <Row>
+                   <div className='col-6 mxr__header-align-left'>
+                        <span class="mxr__header-phone">Call Us Today! (+44) 20 7193 5407</span>
+                        <span class="mxr__header-separator">|</span>
+                        <span class="mxr__header-email">team@mxr.ai</span>
+                    </div>
+                   <div className='col-6 mxr__header-align-right'>
+                       <FaFacebookF size={18}/>
+                       <FaTwitter size={18}/>    
+                       <FaInstagram size={18}/>    
+                       <FaLinkedinIn size={18}/>    
+                    </div>
+                </Row>            
+                </Container>
+            </div>
+            <div className='mxr__header-navbar'>                        
+                {/* <div className='mxr__header-logo'>
+                    <img src={Logo} alt='Meta Extended Reality' />
+                </div> */}
+                <Navbar expand="lg">
+                    <Container>
+                        <Navbar.Brand href="#home"><img src={Logo} className="mxr__header-logo" alt='Meta Extended Reality' /></Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavLink className="nav-link" to="/">Home</NavLink>
+                            <NavLink className="nav-link" to="/about">About</NavLink>
+                            <NavDropdown title="Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                    </Navbar>
+            </div>
+            </div>
+        </div>
+      </>
+      
+  )
+}
+
+export default Header
