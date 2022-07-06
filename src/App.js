92a37c604d8624e4fd16659fba4c80fd3c365de7
simer1991 <satinder0728@gmail.com>
2022-07-06 10:53:28 +0530

    social sharing

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import {
 } from './components';
 import { Home, About, Contact, VR, VRL, MAD, MR, WDD, RI, Php, Laravel, CodeIngenator, Python, Node, Android, Angular, Reactjs, Vue, Mean, Mern, Magento, WooCommerce, Shopify, IOS, ReactNative, Hybrid, Flutter, Ionic, Swift, Worldpress, Drupal, CustomUnrealEngine, UnityAppDevelopment, Project1, Project2, Projects, AR, Technology } from './Pages';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 function App() {
   return (
