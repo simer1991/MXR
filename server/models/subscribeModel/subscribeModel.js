cd05263a2659cbcac48335b4e4fb1da650b6f013
aijaz <ehmmadaejazz33@gmail.com>
2022-09-26 10:01:20 +0530

    changes

---
+++ server/models/subscribeModel/subscribeModel.js
@@ -0,0 +1,12 @@
+// import mongoose from "mongoose";
+
+// const subscribeSchema= new mongoose.Schema({
+//     email:{
+//       type:String,
+//       validate:{
+//         validator:function(v){
+//             return
+//         }
+//       }
+//     }
+// })
